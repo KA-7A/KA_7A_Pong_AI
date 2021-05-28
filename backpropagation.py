cb4a6573048014f69e3380b1f5867c04a1d82a45
ka_7a <galagan.ka@phystech.edu>
2021-05-28 19:42:11 +0300

    /F 2.31

--- backpropagation.py
+++ backpropagation.py
@@ -194,3 +194,34 @@ def back_propagation(train, test, l_rate, n_epoch, n_hidden):
         prediction = predict(network, row)
         predictions.append(prediction)
     return (predictions)
+
+if __name__ == "__main__":
+    n_outputs = 3
+    n_inputs = 5
+    n_hidden = 4
+    network = initialize_network(n_inputs, n_hidden, n_outputs)
+
+    dataset = list()
+
+    train_network(network, dataset, 0.5, 20, n_outputs)
+    inputs = list()
+    res = predict(network, inputs)
+
+    if False:
+        filename = 'seeds_dataset.csv'
+        dataset = load_csv(filename)
+        for i in range(len(dataset[0])-1):
+            str_column_to_float(dataset, i)
+        # convert class column to integers
+        str_column_to_int(dataset, len(dataset[0])-1)
+        # normalize input variables
+        minmax = dataset_minmax(dataset)
+        normalize_dataset(dataset, minmax)
+        # evaluate algorithm
+        n_folds = 5
+        l_rate = 0.3
+        n_epoch = 500
+        n_hidden = 5
+        scores = evaluate_algorithm(dataset, back_propagation, n_folds, l_rate, n_epoch, n_hidden)
+        print('Scores: %s' % scores)
+        print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
